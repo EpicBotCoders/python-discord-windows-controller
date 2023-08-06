5b64def60a816d373c98b05b46c8a423eb771d5f
Suraj S <qwerky2003@gmail.com>
2023-08-06 22:19:34 +0530

    witth unlock

---
+++ bot.py
@@ -0,0 +1,47 @@
+import discord
+import os
+import subprocess
+from commands import unlock as unlock
+
+if(os.path.exists("key.txt")):
+    with open("key.txt","r") as f:
+        TOKEN = f.read()
+else:
+    TOKEN = ""
+
+
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the message content intent
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    msg = str(message.content).lower()
+    if message.author == client.user:
+        return
+    elif message.channel.id != 1137776176451039374:
+        print("Wrong channel")
+        return
+    elif message.author.bot:
+        return
+    elif message.author.id != 637911567920529409:
+        print("Wrong author")
+        return
+
+    if msg == "lock":
+        print(f"Received message from {message.author}: {message.content}")
+
+        # Command to execute
+        command = 'rundll32.exe user32.dll,LockWorkStation'
+        # Execute the command
+        subprocess.run(command, shell=True)
+    if msg == "unlock":
+        pass
+        unlock.exec()
+
+client.run(TOKEN)
