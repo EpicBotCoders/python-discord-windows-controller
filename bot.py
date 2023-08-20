228356a009ec679cda2cfb350bc1f2be49014e53
Suraj S <qwerky2003@gmail.com>
2023-08-20 23:57:10 +0530

    Added internet check at start

--- bot.py
+++ bot.py
@@ -1,49 +1,69 @@
 import discord
 import os
 import json
+import socket
+import asyncio
 
-if os.path.exists("key.txt"):
-    with open("key.txt", "r") as f:
-        TOKEN = f.read()
-else:
-    TOKEN = ""
+def is_connected():
+    try:
+        # Try to resolve a common domain to check if network is available
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False
 
 def load_config():
     with open("config.json", "r") as config_file:
         config = json.load(config_file)
         return config
 
-intents = discord.Intents.default()
-intents.message_content = True
+async def main():
+    while True:
+        if is_connected():
+            break
+        print("Waiting for network...")
+        await asyncio.sleep(5)  # Wait for 5 seconds before checking again
 
-client = discord.Client(intents=intents)
+    if os.path.exists("key.txt"):
+        with open("key.txt", "r") as f:
+            TOKEN = f.read()
+    else:
+        TOKEN = ""
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-@client.event
-async def on_message(message):
-    msg = str(message.content).lower()
-    config = load_config()
+    client = discord.Client(intents=intents)
 
-    if message.author == client.user or message.channel.id != config["command_channel_id"]:
-        return
+    @client.event
+    async def on_ready():
+        print(f"We have logged in as {client.user}")
 
-    if message.author.bot or message.author.id != config["author_id"]:
-        return
-    
-    if str(message.attachments) != "[]":
-        print(message.attachments)
-        from commands.file_save import execute_file_save
-        await execute_file_save(client,message,config)
+    @client.event
+    async def on_message(message):
+        msg = str(message.content).lower()
+        config = load_config()
 
-    if msg == "lock":
-        from commands.lock_command import execute_lock_command
-        await execute_lock_command(client, message, config)
+        if message.author == client.user or message.channel.id != config["command_channel_id"]:
+            return
 
-    if msg == "ping":
-        from commands.ping_command import execute_ping_command
-        await execute_ping_command(client, message, config)
+        if message.author.bot or message.author.id != config["author_id"]:
+            return
 
-client.run(TOKEN)
+        if str(message.attachments) != "[]":
+            print(message.attachments)
+            from commands.file_save import execute_file_save
+            await execute_file_save(client,message,config)
+
+        if msg == "lock":
+            from commands.lock_command import execute_lock_command
+            await execute_lock_command(client, message, config)
+
+        if msg == "ping":
+            from commands.ping_command import execute_ping_command
+            await execute_ping_command(client, message, config)
+
+    await client.start(TOKEN)
+
+asyncio.run(main())
--- commands/ping_command.py
+++ commands/ping_command.py
@@ -1,5 +1,10 @@
+import time
+
 async def execute_ping_command(client, message, config):
     LOG_CHANNEL_ID = config["log_channel_id"]
 
     await message.channel.send("Pong !")
-    await message.guild.get_channel(LOG_CHANNEL_ID).send("Pinged!")
+    sentmsg = await message.guild.get_channel(LOG_CHANNEL_ID).send("Pinged!")
+    await message.delete()
+    time.sleep(5)
+    await sentmsg.delete()
